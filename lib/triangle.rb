b5af8ab2cd58264c6309fbc0cb9b467f70fff433
Jackie <jye0325@gmail.com>
2022-07-26 01:29:56 -0400

    commit tree 233a97051d65c80b385a96b46b4bd61781c314c1

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_reader :length, :width, :height
+  def initialize (l, w, h)
+    @length, @width, @height = l,w,h
+  end
+
+  def inequality
+    length + width > height && length + height > width && height + width > length
+  end
+
+  def kind
+    if length <= 0 || width <=0 || height <= 0
+      raise TriangleError
+    elsif inequality == false
+      raise TriangleError
+    elsif length == width && length == height
+      :equilateral
+    elsif length == width || length == height || width == height
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
